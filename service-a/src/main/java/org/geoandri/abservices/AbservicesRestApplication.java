6cc4d3574a35f8a4072b9d4119b9a5520ca3536c
George Andrinopoulos <geoandri@gmail.com>
2019-12-13 21:13:53 +0200

    Initial commit

---
+++ service-a/src/main/java/org/geoandri/abservices/AbservicesRestApplication.java
@@ -0,0 +1,11 @@
+package org.geoandri.abservices;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class AbservicesRestApplication extends Application {
+}
---
+++ service-b/src/main/java/org/geoandri/abservices/AbservicesRestApplication.java
@@ -0,0 +1,19 @@
+package org.geoandri.abservices;
+
+import org.eclipse.microprofile.auth.LoginConfig;
+
+import javax.annotation.security.DeclareRoles;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+
+@LoginConfig(authMethod = "MP-JWT", realmName = "jwt-jaspi")
+@DeclareRoles({"protected"})
+
+public class AbservicesRestApplication extends Application {
+}
